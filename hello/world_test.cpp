18359a790dfeecc195e677448591d4b4793bc348
Andy Loomis <aloomis@google.com>
2016-02-13 18:58:44 -0800

    Added gmock library and updated example test.

--- hello/world_test.cpp
+++ hello/world_test.cpp
@@ -1,5 +1,27 @@
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-TEST(AlwaysTrue, OneEqualsOne) {
-    EXPECT_EQ(1, 1);
+namespace {
+
+using ::testing::Return;
+
+class World {
+public:
+    virtual ~World() = default;
+    virtual int GetRadius() const = 0;
+};
+
+class WorldMock : public World {
+public:
+    MOCK_CONST_METHOD0(GetRadius, int());
+};
+
+}  // namespace
+
+TEST(WorldTest, MockWorks) {
+    WorldMock world;
+    ON_CALL(world, GetRadius()).WillByDefault(Return(42));
+    EXPECT_CALL(world, GetRadius()).Times(2);
+    EXPECT_EQ(42, world.GetRadius());
+    EXPECT_EQ(42, world.GetRadius());
 }
